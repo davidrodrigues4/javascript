cc218b20198a8faaf493cbf716c7c2ea55602e9c
davidrodrigues4 <155097808+davidrodrigues4@users.noreply.github.com>
2024-03-30 13:56:30 -0300

    exercício hora do dia completo

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,23 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = 'imagens/foto-manha.png'
+        document.body.style.background = '#FDE575'
+    }
+    else if (hora >= 12 && hora < 18) {
+        //boa tarde
+        img.src = 'imagens/foto-tarde.png'
+        document.body.style.background = '#FF8F01'
+    }
+    else {
+        // boa noite
+        img.src = 'imagens/foto-noite.png'
+        document.body.style.background = '#253132'
+    }
+}
+

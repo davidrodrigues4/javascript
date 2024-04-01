a66202eb4a40f53dcffd0bc79df60774778dfed3
davidrodrigues4 <155097808+davidrodrigues4@users.noreply.github.com>
2024-04-01 11:58:08 -0300

    repetições (while e do..while)

---
+++ aula13ex/ambiente.js
@@ -0,0 +1,12 @@
+/*var numero = 1
+while(numero <= 5) {
+    console.log(`Passo ${numero}`)
+    numero++
+}
+*/
+
+var numero = 1
+do {
+    console.log(`Passo ${numero}`)
+    numero++
+} while (numero <= 5)
